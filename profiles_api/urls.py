cc8728250e8a45b301b9a7c5df9676ecba6ab9ea
Hammad Ali <hammad11ali@gmail.com>
2020-10-22 15:51:05 +0500

    email

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,7 @@ router.register('profile', views.UserProfileViewSet)
 urlpatterns = [
     path('login/', views.Login.as_view()),
     path('logout/', views.Logout.as_view()),
+    path('email/', views.EmailConfirmation.as_view()),
     path('user/', include(router.urls)),
     path('', include('contentmanager.urls')),
     # path('concepts/', Concept_View.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,8 +8,10 @@ from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from profiles_api import models
 from profiles_api import serializers
+import uuid
 import datetime
 from django.core.mail import send_mail
+
 
 class Login(ObtainAuthToken):
     """Handle auth token"""
@@ -23,29 +25,39 @@ class Logout(APIView):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
 
+
 class EmailConfirmation(APIView):
     def post(self, request, format=None):
         user_id = request.data['user_id']
         user = models.UserProfile.objects.filter(id=user_id)
         if user.count() > 0:
-            user=user[0]
-            t=user.id+datetime.datetime.now().timestamp()
-            models.EmailToken.objects.create(user=user,token=t,usetype='emailconfirm')
+            user = user[0]
+            t = uuid.uuid3(uuid.NAMESPACE_DNS, 'user'+str(user.id) +
+                           user.email+str(datetime.datetime.now().timestamp()))
+            send_mail('Confirm Email', 'Your account for Kamyab Learning System Has been successfully created. Please open this link to confirm your email  http://127.0.0.1:8000/api/email/?token=' +
+                      str(t), 'hammad22ali@gmail.com', ['hammad11ali@gmail.com'])
+            models.EmailToken.objects.create(
+                user=user, token=t, usetype='emailconfirm')
             return Response({'message': 'ok'})
         else:
             return Response({'message': 'ok'})
+
     def get(self, request, format=None):
         if 'token' in request.query_params.keys():
             token = request.query_params['token']
             emailcheck = models.EmailToken.objects.filter(token=token)
             if emailcheck.count() > 0:
-                emailcheck=emailcheck[0]
-                if(emailcheck.usertype=='emailconfirm'):
-                    models.UserProfile.objects.filter(id=user_id).update(is_enabled=True)
+                emailcheck = emailcheck[0]
+                if(emailcheck.usetype == 'emailconfirm'):
+                    models.UserProfile.objects.filter(
+                        id=emailcheck.user.id).update(is_enabled=True)
+                emailcheck.delete()
                 return Response({'message': 'ok'})
+            else:
+                return Response({'message': 'Invalid Token'})
         else:
-            return Response({'message': 'ok'})
-        
+            return Response({'message': 'No token'})
+
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """handle creating and and updating profiles"""
